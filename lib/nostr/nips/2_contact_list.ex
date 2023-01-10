86305d0bb7f878066d3e19ab776ad6de018f2c48
Amos Newswanger <aonews@gmail.com>
2023-01-10 00:59:33 -0600

    Add contact list parsing

---
+++ lib/nostr/nips/2_contact_list.ex
@@ -0,0 +1,18 @@
+defmodule Nostr.Nips.ContactList do
+  alias Nostr.Event
+  alias Nostr.Crypto
+
+  def get_contact_list %Event{} = event do
+    tags = event.tags
+      |> Enum.filter(fn tag -> 
+        (length(tag) >= 2) and Enum.at(tag, 0) == "p" and Crypto.valid_hex?(Enum.at(tag, 1), 32)
+      end)
+      |> Enum.map(fn tag -> 
+        %{
+          pubkey: Enum.at(tag, 1),
+          relay: Enum.at(tag, 2),
+          petname: Enum.at(tag, 3)
+        }
+      end)
+  end
+end
